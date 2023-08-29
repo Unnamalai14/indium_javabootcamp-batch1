b3426efb8859991e595da4b54ba5324097d0a447
Unnamalai <118506785+Unnamalai14@users.noreply.github.com>
2023-08-29 12:26:52 +0530

    calc

---
+++ assignments/corejava/Calc/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] main){
+        int num1 = 16;
+        int num2 = 4;
+
+        //Addition
+        int num3 = num1+num2;
+        System.out.println("Addition of two numbers : "+num3);
+
+        //Sub
+        int num4 = num1-num2;
+        System.out.println("Subtraction of two numbers : "+num4);
+
+        //multiplication
+        int num5 = num1*num2;
+        System.out.println("Multiplication of two numbers : "+num5);
+
+        //Division
+        int num6 = num1/num2;
+        System.out.println("Division of two numbers : "+num6);
+    }
+}
