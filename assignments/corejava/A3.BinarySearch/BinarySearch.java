799690d57b9ffbf08976ddbbcbcdfad7faffff60
Unnamalai <118506785+Unnamalai14@users.noreply.github.com>
2023-09-04 14:16:53 +0530

    BSearch

--- assignments/corejava/A3.BinarySearch/BinarySearch.java
+++ assignments/corejava/A3.BinarySearch/BinarySearch.java
@@ -1,45 +1,29 @@
+package com.examples.java.arrays;
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
-public class CmdLine
+public class BinarySearch {
 
-{
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-        double num1, num2, res;
-
-        System.out.println("Enter the First Number: ");
-        try
-        {
-            num1 = scan.nextDouble();
+    public static void binarySearch(int arr[], int first, int last, int key){
+        int mid = (first + last)/2;
+        while( first <= last ){
+            if ( arr[mid] < key ){
+                first = mid + 1;
+            }else if ( arr[mid] == key ){
+                System.out.println("Element is found at index: " + mid);
+                break;
+            }else{
+                last = mid - 1;
+            }
+            mid = (first + last)/2;
         }
-        catch(InputMismatchException e)
-        {
-            System.out.println("\nInvalid Input!");
-            System.out.println("Exception Name: " + e);
-            return;
+        if ( first > last ){
+            System.out.println("Element is not found!");
         }
-
-        System.out.println("Enter the Second Number: ");
-        try
-        {
-            num2 = scan.nextDouble();
-        }
-        catch(InputMismatchException e)
-        {
-            System.out.println("\nInvalid Input!");
-            System.out.println("Exception Name: " + e);
-            return;
-        }
-
-        res = num1 + num2;
-        System.out.println("\nAddition Result = " + res);
-        res = num1 - num2;
-        System.out.println("Subtraction Result = " + res);
-        res = num1 * num2;
-        System.out.println("Multiplication Result = " + res);
-        res = num1 / num2;
-        System.out.println("Division Result = " + res);
+    }
+    public static void main(String args[]){
+        int arr[] = {10,20,30,40,50};
+        int key = 30;
+        int last=arr.length-1;
+        binarySearch(arr,0,last,key);
     }
 }
